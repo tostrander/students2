99ce918b37269d61fb7f7c221624109352d2fe1a
Tina Ostrander <tostrander@greenriver.edu>
2018-02-15 17:00:21 -0500

    Added new student form

--- index.php
+++ index.php
@@ -27,5 +27,44 @@ $f3->route('GET /', function($f3, $params) {
 
 });
 
+//Define a default route
+$f3->route('GET|POST /add', function($f3, $params) {
+
+    /*
+    [sid] => 3333
+    [last] => Iqbal
+    [first] => Shahbaz
+    [birthdate] => 2000-05-05
+    [gpa] => 4.0
+    [advisor] => 2
+    [submit] => Submit
+    */
+
+    if (isset($_POST['submit'])) {
+
+        //get the form data
+        $sid = $_POST['sid'];
+        $last = $_POST['last'];
+        $first = $_POST['first'];
+        $birthdate = $_POST['birthdate'];
+        $gpa = $_POST['gpa'];
+        $advisor = $_POST['advisor'];
+
+        //validate data
+
+        //insert the student into the db
+        $result = insertStudent($sid, $last, $first, $birthdate, $gpa, $advisor);
+
+        //redirect to home page
+        $f3->reroute('/');
+    }
+
+
+    //load a template
+    $template = new Template();
+    echo $template->render('views/add-student.html');
+
+});
+
 //Run fat free
 $f3->run();
--- model/db-functions.php
+++ model/db-functions.php
@@ -34,5 +34,29 @@ function getStudents()
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
     //print_r($result);
     return $result;
+}
 
+function insertStudent($id, $last, $first, $birthdate, $gpa, $advisor)
+{
+    global $dbh;
+
+    //1. Define the query
+    $sql = "INSERT INTO student VALUES (:id, :last, :first, :birthdate, :gpa, :advisor);";
+
+    //2. Prepare the statement
+    $statement = $dbh->prepare($sql);
+
+    //3. Bind parameters
+    $statement->bindParam(':id', $id, PDO::PARAM_STR);
+    $statement->bindParam(':last', $last, PDO::PARAM_STR);
+    $statement->bindParam(':first', $first, PDO::PARAM_STR);
+    $statement->bindParam(':birthdate', $birthdate, PDO::PARAM_STR);
+    $statement->bindParam(':gpa', $gpa, PDO::PARAM_STR);
+    $statement->bindParam(':advisor', $advisor, PDO::PARAM_STR);
+
+    //4. Execute the query
+    $result = $statement->execute();
+
+    //5. Return the result
+    return $result;
 }
